2218f2eb41ab2e73b77e51e1691ee54e5e022a18
cuongtvph31835 <cuongtvph31835@fpt.edu.vn>
2023-11-20 21:43:57 +0700

    trangchu

---
+++ huongdan.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/huongdan.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <div class="header">
+            <div class="header1">
+                <img class="a" src="images/istockphoto-1264235523-612x612.jpg" alt="">
+                <h5>0926524733</h5>
+            </div>
+            <div class="header1">
+                <div class="header1">
+                    <img class="a" src="images/tải xuống.png" alt="">
+                    <a href="login.html">Tài khoản</a>
+                </div>
+                <div class="header1">
+                    <img class="a" src="images/tải xuống (1).png" alt="">
+                    <a href="giohang.html">Giỏ hàng</a>
+                </div>
+            </div>
+        </div>
+        <div class="header2">
+            <div class="logo">
+                <img class="b" src="images/logo.jpg" alt="">
+            </div>
+            <div class="menu">
+                <ul>
+                    <li><a href="trangchu.html"><b> Trang chủ</b></a></li>
+                    <li><a href="aoxuanhe.html"><b>Áo xuân hè</b> </a></li>
+                    <li><a href="quan.html"><b>Quần</b> </a></li>
+                    <li><a href="thongtin.html"><b> Tin tức</b></a></li>
+                </ul>
+            </div>
+            <div class="tim">
+                <input class="timkiem" type="text" placeholder="Tìm kiếm" style="border: 1px solid black; border-radius: 10px;">
+            <a href="#"><img class="icontim" src="images/images.png" alt=""></a>
+            </div>
+        </div>
+        <div class="nav">
+            <a href="thongtin.html"><h1> < back </h1></a>
+            <h2>Hướng Dẫn Mua Hàng</h2>
+        </div>
+        <center><div class="ke" ></div></center>
+        <div class="conten">
+            <b style="margin: 20px;">( Nhận ship với đơn >150K, Freeship với đơn hàng >400K )</b><br><br>
+            <b style="margin: 20px;">1. Khu vực Hà Nội:</b><br><br> 
+            <p>* Với các bạn ở HN có thể đến trực tiếp cửa hàng 110 Phố Nhổn, 326 Cầu Giấy, 
+                154 Quang Trung , 131 Cầu Giấy, 225 Bạch Mai , 49 Chùa Bộc (mở cửa từ 8:30 am- 22:30 pm)
+                 để giao dịch trực tiếp.</p>
+            <p>* Các bạn ở HN nhưng không có có điều kiện qua xem trực tiếp có thể liên hệ với bên mình để đc vận chuyển và thanh toán trực 
+                tiếp tại nhà bằng cách inbox trực tiếp fb hoặc chat trực tiếp trên website Shop: <a style="color: #0057FF;" href="https://atino.vn/"> https://atino.vn/</a> </p>
+            <p>(Địa chỉ vs số đt người nhận + Mẫu áo +Mầu áo+ size hoặc chiều cao cân nặng -->inbox tại <a style="color: #0057FF;" href="https://www.facebook.com/ShopDoc123/">https://www.facebook.com/ShopDoc123/</a>  hoặc gửi đơn hàng trên web )</p>
+            <p>(Giá trị thanh toán = giá trị hàng + 30K tiền phí ship tùy theo cân nặng)</p><br>
+            <b>Khoảng 1-3 ngày các bạn sẽ nhận được hàng !</b><br><br>
+            <b>2. Khu vực các tỉnh khác ko phải HN</b><br>
+            <p>* Với các bạn ở TP hoặc Thị Xã thì cac bạn inbox trực tiếp cho Shop: Mã sp + mầu + size ( hoặc chiều cao cân nặng ) + địa chỉ liên hệ và số đt của bạn . Khi nhận đc tin nhắn inbox Shop sẽ trưc tiếp liên hệ lại để xác nhận đơn hàng
+                 với các bạn rồi chuyển hàng thanh toán COD (Thanh toán trực tiếp khi giao nhận hàng - phí 40K )</p><br>
+            <b>Cách thức thanh toán chuyển khoản</b><br>
+            <p>Bước 1: Thông tin sản phẩm
+                - Các bạn inbox trưc tiếp trên fb: <a href="https://www.facebook.com/ShopDoc123/" style="color: #0057FF;" > https://www.facebook.com/ShopDoc123/</a><br><br>
+                
+                (mục tin nhắn ) để hỏi thông tin sp mẫu mã đó còn hay hết hàng . 
+                - Hoặc chát trưc tuyến trên Web site Shop : <a href="https://shopdoc.vn/" style="color: #0057FF;"> https://shopdoc.vn/</a><br><br>
+                
+                Bước 2: Chuyển khoản vào tài khoản Chủ tài khoản: LÊ THIÊN BÌNH <br><br>
+                
+                + Techcombank : 19028655351046 ( Chi nhánh Từ Liêm HN)</p><br>
+            <b>==> Sau khi chuyển khoản các bạn có thể chụp lại ảnh hoá đơn rồi gửi inbox cho Shop để Shop chuyển hàng sớm cho bạn .</b><br><br>
+            <b>Khoảng 3-5 ngày là hàng đến nơi (Nếu các bạn ở TP- còn ở Huyện sẽ chậm hơn 1-2ngày )! <br>  (trừ T7 - Chủ Nhật bên vận chuyển nghỉ)</b>
+        </div>
+        <div class="footer">
+            <span class="f">Đăng ký ngay để đc nhận thông báo khi có sản phẩm mới</span>
+            <form class="regiter">
+                <input style="border: 1px solid black;"
+                  class="input-register"
+                  type="email"
+                  required
+                  placeholder="Email Address"
+                /><br />
+                <input style="border: 1px solid black;"
+                  type="submit"
+                  value="Đăng ký"
+                  id="submit"
+                  class="btn-register-footer"
+                />
+              </form>
+        </div>
+    </div>
+</body>
+</html>
---
+++ thongtin.php
@@ -0,0 +1,37 @@
+<?php
+ include "view/header.php";
+?>
+        <h1>THÔNG TIN</h1>
+        <div class="nav">
+            <div class="hd">
+                    <img src="upload/tin2.jpg" alt="">
+                   <a href="trangchu.php?act=huongdan.php"> <span><b>Hướng dẫn mua hàng</b><p>Cách thức xem và đặt hàng</p></span></a>
+            </div>
+        </div>
+        <center><div class="ke" ></div></center>
+        <div class="nav">
+            <div class="hd">
+                    <img src="upload/tin.jpg" alt="">
+                   <a href="trangchu.php?act=cs.php"> <span><b>Chính sách vận chuyển</b><p>Vận chuyển</p></span></a>
+            </div>
+        </div>
+        <!-- <div class="footer">
+            <span class="f">Đăng ký ngay để đc nhận thông báo khi có sản phẩm mới</span>
+            <form class="regiter">
+                <input style="border: 1px solid black;"
+                  class="input-register"
+                  type="email"
+                  required
+                  placeholder="Email Address"
+                /><br />
+                <input style="border: 1px solid black;"
+                  type="submit"
+                  value="Đăng ký"
+                  id="submit"
+                  class="btn-register-footer"
+                />
+              </form>
+        </div> -->
+    </div>
+</body>
+</html>

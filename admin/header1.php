9c3bebad23e9e415ce30cdf7ae63c431a7afa3f1
hieunvph37601 <hieunvph37601@fpt.edu.vn>
2023-11-20 22:17:20 +0700

    update

--- admin/header1.php
+++ admin/header1.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Du an 1</title>
-    <link rel="stylesheet" href="../view/css/css.css">
+    <title>Dự án mẫu</title>
+    <link rel="stylesheet" href="../css/css.css">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet">
     <style>
      

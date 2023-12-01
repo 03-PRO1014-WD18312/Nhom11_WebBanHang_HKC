043fdf636d85fb0014a3afd4513978cae49b5b09
cuongtvph31835 <cuongtvph31835@fpt.edu.vn>
2023-12-01 23:39:58 +0700

    a

--- index.php
+++ index.php
@@ -128,16 +128,16 @@ if (isset($_GET['act']) && ($_GET['act'] != "")) {
             case 'giohang1':
                 include "view/giohang.php";
                 break;
-        case 'giohang':
-            if (isset($_GET['del']) && ($_GET['del'] == 1)) {
-                unset($_SESSION["giohangcuatoi"]);
-                // $_SESSION['giohangcuatoi']=[];
-                header('location: index.php');
-            }else {
-                if (isset($_SESSION['giohangcuatoi'])) {
-                    $tongdh=get_tongdh();
-                }
-                include "view/giohang.php";
+            case 'giohang':
+                if (isset($_GET['del']) && ($_GET['del'] == 1)) {
+                    unset($_SESSION["giohangcuatoi"]);
+                    // $_SESSION['giohangcuatoi']=[];
+                    header('location: index.php');
+                } else {
+                    if (isset($_SESSION['giohangcuatoi'])) {
+                        $tongdh = get_tongdh();
+                    }
+                    include "view/giohang.php";
             }
             break;
 

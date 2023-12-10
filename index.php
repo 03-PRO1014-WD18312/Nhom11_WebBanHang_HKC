ce73631f641f658c0fee0063d4d325854151c463
cuongtvph31835 <cuongtvph31835@fpt.edu.vn>
2023-12-10 18:13:04 +0700

    1

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ if (!isset($_SESSION['donhangcuatoi'])) $_SESSION['donhangcuatoi'] = [];
 $spnew = loadall_sanpham_home();
 $dsdm = loadall_danhmuc();
 $dstop10 = loadall_sanpham_top10();
-$kiemtra=0;
+$kiemtra = 0;
 if (isset($_GET['act'])&&$_GET['act'] == 'donhang'||$_GET['act'] == 'bill'){
     $kiemtra = 1;
 }
